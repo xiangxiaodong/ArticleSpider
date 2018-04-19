477b0d7880ddbfa7998b11a654c30cfb4d32296b
MaxPower1024 <xd102401@163.com>
2018-04-19 18:13:18 +0800

    Revert "Step 4"

--- ArticleSpider/items.py
+++ ArticleSpider/items.py
@@ -6,87 +6,14 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy.loader.processors import MapCompose, TakeFirst
-import datetime
-from scrapy.loader import ItemLoader
-from scrapy.loader.processors import Join
-import re
 
 
 class ArticlespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    
+
     pass
 
-<<<<<<< HEAD
-
-# 过滤函数
-def add_jobbole(value):
-    return value + '-jobbole'
-
-
-# 转换日期函数
-def date_convert(value):
-    try:
-        create_date = datetime.datetime.strptime(value, '%Y/%m/%d').date()
-    except Exception as e:
-        create_date = datetime.datetime.now().date()
-    return create_date
-
-
-class ArticleItemLoader(ItemLoader):
-    # 自定义itemloader
-    default_output_processor = TakeFirst()
-
-
-def get_nums(value):
-    match_re = re.match(".*?(\d+).*", value)
-    if match_re:
-        nums = int(match_re.group(1))
-    else:
-        nums = 0
-    return nums
-
-
-def remove_comment_tags(value):
-    # 去掉tags中提取的评论两个字
-    if '评论' in value:
-        return ''
-    else:
-        return value
-
-
-def return_value(value):
-    return value
-
-
-class JobBoleArticleItem(scrapy.Item):
-    title = scrapy.Field()
-    create_date = scrapy.Field(
-        input_processor=MapCompose(date_convert)
-    )
-    url = scrapy.Field()
-    url_object_id = scrapy.Field()
-    front_image_url = scrapy.Field(
-        output_processor = MapCompose(return_value)
-    )
-    front_image_path = scrapy.Field()
-    praise_nums = scrapy.Field(
-        input_processor=MapCompose(get_nums)
-    )
-    comment_nums = scrapy.Field(
-        input_processor=MapCompose(get_nums)
-    )
-    fav_nums = scrapy.Field(
-        input_processor=MapCompose(get_nums)
-    )
-    tags = scrapy.Field(
-        input_processor=MapCompose(remove_comment_tags),
-        output_processor=Join(',')
-    )
-    content = scrapy.Field()
-=======
 # class JoBoleArticleItem(scrapy.Item):
 #     title = scrapy.Field()
 #     create_date = scrapy.Field()
@@ -100,4 +27,3 @@ class JobBoleArticleItem(scrapy.Item):
 #     content = scrapy.Field()
     
     
->>>>>>> master
